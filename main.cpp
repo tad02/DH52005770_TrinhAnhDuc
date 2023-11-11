73848e7c01a94b200fad8902c966658c90937699
tad02 <qnce02@gmail.com>
2023-11-11 16:24:52 +0700

    nhap so

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -6,7 +7,9 @@ int main(){
  int num;
  cout <<"Nhap vao 1 so nguyen: ";
  cin >> num;
-
+ if(kiemTraSoChinhPhuong(num)){
+   cout<<"\nSo chinh phuong";
+ }
 
  return 0;
 }
