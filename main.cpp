0526f71151753d36b29c4af20355f4ef965fc5fe
tad02 <qnce02@gmail.com>
2023-11-11 16:26:33 +0700

    hoanthien

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
 using namespace std;
-
+bool kiemTraSoHoanThien(int num){
+	int n = 0;
+	for(int i = 1 ; i < num ;i++)
+		if(num%i==0)
+			n+=i;
+return num==n; 
+}
 int main(){
  int num;
  cout <<"Nhap vao 1 so nguyen: ";
